ee38d48d3b62f0abb8893d0df2e4d8e3e344a66a
Alexander Demin <alexander.demin@onalytica.com>
2019-04-16 18:19:54 +0100

    Add test_16_line

--- tcp_proxy_test.js
+++ tcp_proxy_test.js
@@ -23,6 +23,16 @@ const hexify = require('./hexify');
   assert.equal(a, e)
 })();
 
+(function test_16_line() {
+  console.log(arguments.callee.name)
+  a = hexify.hexify(Buffer.from([...Array(16).keys()]), 0xC0DE)
+  e = 
+    '######## ######## 00.01.02.03.04.05.06.07.08.09.0A.0B.0C.0D.0E.0F\n' +
+    '-------- -------- -----------------------------------------------\n' +
+    '0000C0DE 00000000 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F |................|'
+  assert.equal(a, e)
+})();
+
 (function test_print_only_visible_characters() {
   console.log(arguments.callee.name)
   a = hexify.hexify(Buffer.from([0, 1, 0x1f, 0x20, 0x31, 0x41, 0x7e, 0x7f]), 0xBEEF)
